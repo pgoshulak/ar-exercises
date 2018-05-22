2ace3b817584d7841c6f55ca9e9a2ee18e154dea
Peter Goshulak <pgoshulak@gmail.com>
2018-05-22 14:05:01 -0400

    Complete exercise 6 - add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,37 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
+
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
+
+@store1.employees.create(
+  first_name: "Khurram",
+  last_name: "Virani",
+  hourly_rate: 60
+)
+@store1.employees.create(
+  first_name: "Bruce",
+  last_name: "Wayne",
+  hourly_rate: 50
+)
+@store2.employees.create(
+  first_name: "Clark",
+  last_name: "Kent",
+  hourly_rate: 30
+)
+@store2.employees.create(
+  first_name: "Tony",
+  last_name: "Stark",
+  hourly_rate: 70
+)
+@store2.employees.create(
+  first_name: "Peter",
+  last_name: "Parker",
+  hourly_rate: 50
+)
